68c92d5adf362255a0bd964dc5bcbc96f13e4839
Eddie018 <eddiekamau018@gmail.com>
2024-03-12 08:27:09 +0300

    Learning python

---
+++ python+C/Python+C.py
@@ -0,0 +1,37 @@
+#learning variables
+
+"""
+Commenting
+"""
+
+x=5
+y='Caleb'
+price=60.5
+print(x+price)
+
+#casting 1.integer 2.string 3.float
+#outputing
+x=3
+x="3"
+x=3.0
+
+print(x)
+print(type(x))
+
+#Case sensitivity
+
+price=20
+Price=40
+PRICE=60
+pricE=80
+print(PRICE)
+
+x, y, z= "orange", "cherry", "banana"
+print(x)
+print(y)
+print(z)
+
+x=y=z="120"
+print(x)
+print(y)
+print(z)
